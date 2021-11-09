88198891752f9e58c741f490794c5b8b275fe6b2
Daniil <dilanilka42@gmail.com>
2021-11-09 15:05:30 +0300

    Fixed reversing algorithm

--- app/src/main/java/com/velvet/task1/AnagramCreator.java
+++ app/src/main/java/com/velvet/task1/AnagramCreator.java
@@ -1,74 +1,25 @@
 package com.velvet.task1;
 
-import android.util.Log;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AnagramCreator {
     static public String create(CharSequence input, CharSequence ignored) {
-        StringBuilder inputSB = new StringBuilder(input);
-        StringBuilder ignoredSB = new StringBuilder(ignored);
-        ignoredSB.append(" ");
+        String[] inputArray = input.toString().split("");
+        String[] ignoredArray = ignored.toString().split("");
         StringBuilder output = new StringBuilder();
-        if (inputSB.length()>=1) {
-            int tempReverted = 0;
-            int tempConst = 0;
-            String previous1 = "N";
-            for (int i = 0; i < inputSB.length(); i++) {
-                if (ignoredSB.indexOf(Character.toString(inputSB.charAt(i))) != -1) {
-                    if ((previous1 == "R") || (previous1 == "N")) {
-                        tempConst++;
-                        previous1 = "C";
-                    }
-                }
-                else {
-                    if ((previous1 == "C") || (previous1 == "N")) {
-                        tempReverted++;
-                        previous1 = "R";
-                    }
-                }
+        int cursor = 0;
+        int wordEnd = 0;
+        while (cursor < inputArray.length) {
+            if ((Arrays.asList(ignoredArray).contains(inputArray[cursor]))||(inputArray[cursor].equals(" "))) {
+                wordEnd = cursor;
+                output.insert(wordEnd,inputArray[cursor]);
+                cursor++;
+                wordEnd = cursor;
             }
-            int tempCounter = 0;
-            String previous2 = "N";
-            StringBuilder[] wordArray = new StringBuilder[tempConst + tempReverted];
-            for (int i = 0; i < wordArray.length; i++) {
-                wordArray[i] = new StringBuilder("");
+            else {
+                output.insert(wordEnd,inputArray[cursor]);
+                cursor++;
             }
-            for (int i = 0; i < inputSB.length(); i++) {
-                if (ignoredSB.indexOf(Character.toString(inputSB.charAt(i))) != -1) {
-                    if ((previous2 == "C") || (previous2 == "N")) {
-                        wordArray[tempCounter].append(inputSB.charAt(i));
-                        previous2 = "C";
-                    }
-                    else {
-                        tempCounter++;
-                        wordArray[tempCounter].append(inputSB.charAt(i));
-                        previous2 = "C";
-                    }
-                }
-                else {
-                    if ((previous2 == "R") || (previous2 == "N")) {
-                        wordArray[tempCounter].append(inputSB.charAt(i));
-                        previous2 = "R";
-                    }
-                    else {
-                        tempCounter++;
-                        wordArray[tempCounter].append(inputSB.charAt(i));
-                        previous2 = "R";
-                    }
-                }
-            }
-            for (int i = 0; i < wordArray.length; i++) {
-                if (ignoredSB.indexOf(Character.toString(wordArray[i].charAt(0))) != -1) {
-                    output.append(wordArray[i].toString());
-                }
-                else {
-                    output.append(wordArray[i].reverse().toString());
-                }
-            }
-        }
-        else {
-            output.append(inputSB);
         }
         return output.toString();
     }
--- app/src/main/java/com/velvet/task1/MainActivity.java
+++ app/src/main/java/com/velvet/task1/MainActivity.java
@@ -8,8 +8,6 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
