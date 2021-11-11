c34ea853d5a9a00388c2f000c69eb5c90b79294e
Daniil <dilanilka42@gmail.com>
2021-11-11 18:47:22 +0300

    Edited test

--- app/src/test/java/com/velvet/task1/AnagramCreatorTest.java
+++ app/src/test/java/com/velvet/task1/AnagramCreatorTest.java
@@ -1,13 +1,13 @@
 package com.velvet.task1;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class AnagramCreatorTest {
     @Test
     public void create() {
-        Assert.assertSame(AnagramCreator.create("a1bcd efglh","xl"),"dcb1a hgfle ");
+        Assert.assertEquals(AnagramCreator.create("a1bcd efglh","xl"),"dcb1a hgfle ");
+        Assert.assertEquals(AnagramCreator.create("abcd efgh","xl"),"dcba hgfe ");
     }
+
 }
