62a87d517e1dca5b56f3829760a6d242d4c711ba
Daniil <dilanilka42@gmail.com>
2021-11-12 00:01:28 +0300

    Edited tests

--- app/src/androidTest/java/com/velvet/task1/MainActivityTest.java
+++ app/src/androidTest/java/com/velvet/task1/MainActivityTest.java
@@ -21,9 +21,9 @@ public class MainActivityTest {
     @Test
     public void testUserInputScenario() {
 
-        String[] input = new String[]{"London","Manchester", "Lancaster", "Marsh St., Innsmouth", "11.11.2021", "X Æ A-12"};
-        String[] ignored = new String[]{"n","ae", "Lr", ".,", "1", "12"};
-        String[] output = new String[]{"odnoLn","ratshecneM", "Letsacnar", "hsraM tS., htuomsnnI", "1121102..1", "X Æ -A12"};
+        String[] input = new String[]{"London","Manchester", "Lancaster", "Marsh St., Innsmouth", "11.11.2021", "X Æ A-12","Mongolia"};
+        String[] ignored = new String[]{"n","ae", "Lr", ".,", "1", "12",""};
+        String[] output = new String[]{"odnoLn","ratshecneM", "Letsacnar", "hsraM tS., htuomsnnI", "1121102..1", "X Æ -A12","ailognoM"};
         for (int i = 0; i < 6; i++) {
             Espresso.onView(withId(R.id.anagramInput)).perform(replaceText(input[i]));
             Espresso.onView(withId(R.id.anagramIgnore)).perform(replaceText(ignored[i]));
--- app/src/test/java/com/velvet/task1/AnagramCreatorTest.java
+++ app/src/test/java/com/velvet/task1/AnagramCreatorTest.java
@@ -2,18 +2,27 @@ package com.velvet.task1;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 public class AnagramCreatorTest {
     @Test
     public void create() {
-        String[] input = new String[]{"London","Manchester", "Lancaster", "Marsh St., Innsmouth", "11.11.2021", "X Æ A-12"};
-        String[] ignored = new String[]{"n","ae", "Lr", ".,", "1", "12"};
-        String[] output = new String[]{"odnoLn","ratshecneM", "Letsacnar", "hsraM tS., htuomsnnI", "1121102..1", "X Æ -A12"};
+        String[] input = new String[]{"London","Manchester", "Lancaster", "Marsh St., Innsmouth", "11.11.2021", "X Æ A-12","Mongolia"};
+        String[] ignored = new String[]{"n","ae", "Lr", ".,", "1", "12",""};
+        String[] output = new String[]{"odnoLn","ratshecneM", "Letsacnar", "hsraM tS., htuomsnnI", "1121102..1", "X Æ -A12","ailognoM"};
         for (int i = 0; i < 6; i++) {
             Assert.assertEquals(AnagramCreator.create(input[i],ignored[i]),output[i]);
         }
-        Assert.assertEquals(AnagramCreator.create(null,"12345"),"");
-        Assert.assertEquals(AnagramCreator.create("12345",null),"12345");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkIsExceptionOfRightType_whenFirstArgumentIsNull() {
+        AnagramCreator.create(null,"12345");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkIsExceptionOfRightType_whenSecondArgumentIsNull() {
+        AnagramCreator.create("12345",null);
     }
 
 }
